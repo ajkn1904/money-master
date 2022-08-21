90da3563165591784bcb5e81e1de3d81293eca22
ajkn1904 <ajknishat@gmail.com>
2022-08-21 06:02:22 +0600

    update

--- JS/moneyCalculation.js
+++ JS/moneyCalculation.js
@@ -1,18 +1,26 @@
 const incomeField = document.getElementById("income-field");
-const incomeValue = incomeField.value;
 
 const foodField = document.getElementById("food");
-const foodValue = foodField.value;
 
 const rentField = document.getElementById("rent");
-const rentValue = rentField.value;
 
 const clothesField = document.getElementById("clothes");
-const clothesValue = clothesField.value;
+
+
 
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
+    
+    const incomeValue = incomeField.value;
+    const foodValue = foodField.value;
+    const rentValue = rentField.value;
+    const clothesValue = clothesField.value;
 
+
+    foodField.value ='';
+    rentField.value = '';
+    clothesField.value = '';
+     
     const totalExpense = parseFloat(foodValue) + parseFloat(rentValue) + parseFloat(clothesValue);   
 
     const previousTotalString = document.getElementById('total-expense');    
@@ -32,5 +40,37 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
 
 });
 
+
+
+
+document.getElementById('save-btn').addEventListener('click', function(){
+   
+    const saveAmountField = document.getElementById('save-amount');
+    const saveAmount = saveAmountField.innerText;
+    
+
+    const saveField = document.getElementById('save-field');
+    const savePercentage = parseFloat(saveField.value);
+    saveField.value = '';
+
+    const remaingBalanceAmountField = document.getElementById('remaining-balance');
+    const remaingBalanceAmount = remaingBalanceAmountField.innerText;
+
+
+    const previousBalanceString = document.getElementById('balance');
+    const previousBalance = parseFloat(previousBalanceString.innerText);
+
+
+    const incomeValue = parseFloat(incomeField.value);
+    incomeField.value = '';
+
+
+    const savingAmountValue = (incomeValue*(savePercentage/100));
+    saveAmountField.innerText = savingAmountValue;
+
+    const remaingBalance = previousBalance - parseFloat(savingAmountValue);
+
+    remaingBalanceAmountField.innerText = remaingBalance;
+});
 
 
